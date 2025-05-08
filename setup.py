b776c79434736a06e3ee1dfcd444837b72841093
SANTIAGO MORALES HENAO <raul.morales@est.iudigital.edu.co>
2025-05-08 03:55:47 +0000

    CREACIÓN DE SETUP.PY

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name="edu_bigdata",
+    author="RAUL SANTIAGO MORALES HENAO",
+    author_email="raul.morales@est.iudigital.edu.co",
+    description="BIGDATA ACTIVIDAD",
+    install_requires=[
+        "pandas",
+        "openpyxl",
+        "requests",
+        "BeautifulSoup4"
+    ] 
+)
