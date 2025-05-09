a3c875868bf820cf24771e80ae2345a86ffe1b23
SANTIAGO MORALES HENAO <raul.morales@est.iudigital.edu.co>
2025-05-09 02:11:42 +0000

    debuger

--- src/edu_bigdata/static/database.py
+++ src/edu_bigdata/static/database.py
@@ -6,18 +6,29 @@ import pandas as pd
 
 class DataBase:
     def __init__(self):
-        self.db_name = "src/edu_bigdata/static/db/dolar_analisis.db" #/workspaces/bigdata_2025_1_2/src/edu_bigdata/static/db
+        self.db_name = "src/edu_bigdata/static/db/dolar_analisis.sqlite" #/workspaces/bigdata_2025_1_2/src/edu_bigdata/static/db
 
-        self.create_database()
-    
-    def create_database(self):
+
+    # CRUD C = create(insert) R= read U = update DF= Delete
+    def insert_data(self,df = pd.DataFrame(),nom_table="dolar_analisis"):
         try:
-            self.conn = sqlite3.connect(self.db_name)
-            self.cursor = self.conn.cursor()
-
-        except Exception as err:
-            print("error al crear la base de datos")
-            # df.sql(self.conn,nom_table)
-
-    def close_database(self):
-        self.conn.close()
+            df = df.copy()
+            conn = sqlite3.connect(self.db_name)
+            df.to_sql(name=nom_table,con=conn,if_exists='replace') # sobreescriba , inserte al final, actualizacion datos
+            conn.close()
+        except Exception as errores:
+            print("error al guradar los datos")
+    
+    def read_data(self,nom_table=""):
+        df=pd.DataFrame()
+        try:
+            if len(nom_table)>0:
+                conn = sqlite3.connect(self.db_name)
+                query= "select * from {}".format(nom_table)
+                df = pd.read_sql_query(sql=query,con=conn)
+                print("*************** consulta base datos tabla: {}*********".format(query))
+                conn.close
+                return df
+        except Exception as errores:
+            print("error al obtener los datos")
+            return df
--- src/edu_bigdata/static/main.py
+++ src/edu_bigdata/static/main.py
@@ -10,11 +10,18 @@ def main():
     database = DataBase()
     df = dataweb.obtener_datos()
     df = dataweb.convertir_numericos(df)
-    print("********** impresion de los datos obtenidos **********")
+    print("*************** imprecion de los datos obtenidos ************************")
     print(df.shape)
     print(df.head())
     df.to_csv("src/edu_bigdata/static/csv/data_web.csv", index=False) #/workspaces/bigdata_2025_1_2/src/edu_bigdata/static/csv
-    database.close_database()
+    nombre_tabla = "dolar_analisis"
+    database.insert_data(df,nombre_tabla)
+    print("*************** Insertar los datos obtenidos en la base datos tabla: {}*********".format(nombre_tabla))
+    print(df.shape)
+    print(df.head())
+    df_2 = database.read_data(nombre_tabla)
+    print(df_2.shape)
+    print(df_2.head())
 
 
 if __name__ == "__main__":
